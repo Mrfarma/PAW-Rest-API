386e0af3f230613c360b3456b320aec413f45f73
Mrfarma <192111237@students.mikroskil.ac.id>
2021-11-20 19:38:06 +0700

    Latihan 7.2

--- Pert 7/index.js
+++ Pert 7/index.js
@@ -1,10 +1,28 @@
-var http = require("http");
+//Latihan 7.2
+// var http = require("http");
 
-http
-  .createServer((req, res) => {
-    res.write("<p>Pengembangan Aplikasi Web dengan Nodejs</p>");
-    res.end();
-  })
-  .listen(8000, () => {
-    console.log("Server is running on port 8000");
-  });
+// http
+//   .createServer((req, res) => {
+//     if (req.url == "/home") res.write("<p>Konten Home</p>");
+//     else if (req.url == "/product") res.write("<p>Konten Product</p>");
+//     else if (req.url == "/order") res.write("<p>Konten Order</p>");
+//     else if (req.url == "/")
+//       res.write("<p>Pengembangan Aplikasi Aplikasi Web dengan Nodejs</p>");
+//     else res.write("<h1>404</h1>");
+//     res.end();
+//   })
+//   .listen(8000, () => {
+//     console.log("Server is running on port 8000");
+//   });
+
+//Latihan 7.1
+// var http = require("http");
+
+// http
+//   .createServer((req, res) => {
+//     res.write("<p>Pengembangan Aplikasi Web dengan Nodejs</p>");
+//     res.end();
+//   })
+//   .listen(8000, () => {
+//     console.log("Server is running on port 8000");
+//   });
